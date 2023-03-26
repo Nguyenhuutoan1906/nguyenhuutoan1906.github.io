062a15f2d480cdc233acd5a57734ef8d7c67b3c2
Nguyễn Hữu Toàn <127460883+Nguyenhuutoan1906@users.noreply.github.com>
2023-03-26 21:28:52 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleicon = document.querySelector('.toggle-icon');
-
-toggleicon.addEventListener('click', () => {
-    toggleicon.classList.toggle('bx-sun');
-    document.body.classList.toggle('dark-mode');
-});
